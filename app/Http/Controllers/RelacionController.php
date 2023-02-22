545a1ac7954fcd95ec33c1dec846a720b06f7541
johanlatorrre <johanlatorre621f@gmail.com>
2023-02-22 18:58:58 -0500

    compacto

---
+++ app/Http/Controllers/RelacionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use App\Models\Producto;
+
+use Illuminate\Http\Request;
+
+
+
+class RelacionController extends Controller
+{
+    public function index(){
+    $productos = Producto::all();
+    return view('Welcome', compact('productos'));
+}
+}
